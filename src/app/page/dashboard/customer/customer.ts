acebc8950edfc5a76040e3eb284ead2b07e9b7f7
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-19 15:13:13 +0530

    feat: Integrate SweetAlert2 for customer addition notifications and enhance customer form with two-way data binding

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -2,16 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../../../model/type';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-customer',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
 export class Customer implements OnInit {
     customerList:Array<CustomerModel>=[];
+    customerObj:CustomerModel ={
+      custID: '',
+      custTitle: '',
+      custName: '',
+      custAddress: '',
+      city: '',
+      province: '',
+      postalCode: '',
+      dob: new Date(),
+      salary: 0.0
+    }
 
     constructor(private http:HttpClient, private cdr:ChangeDetectorRef){
     
@@ -19,6 +32,20 @@ export class Customer implements OnInit {
     ngOnInit(): void {
       this.getall();
     }
+
+    addCustomer(): void{
+      this.http.post("http://localhost:8080/customer/addcustomer", this.customerObj).subscribe(data=>{
+        console.log(data);
+        if(data=true){
+          Swal.fire({
+                title: "Good job!"+this.customerObj.custName,
+                text: "You clicked the button!",
+                icon: "success"
+          });
+        }
+        this.getall();
+    })
+  }
 
     getall(){
       this.http.get<CustomerModel[]>("http://localhost:8080/customer/getall").subscribe(data=>{
