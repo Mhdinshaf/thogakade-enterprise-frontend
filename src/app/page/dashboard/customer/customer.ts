a84e0f0ad6aa9f844be5e684d42911c02f5dc5f4
Mohomed Inshaf <164790727+Mhdinshaf@users.noreply.github.com>
2026-02-19 11:38:37 +0530

    feat: Enhance Customer component with data fetching and improved table layout

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CustomerModel } from '../../../../model/type';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
-export class Customer {
+export class Customer implements OnInit {
+    customerList:Array<CustomerModel>=[];
+
+    constructor(private http:HttpClient, private cdr:ChangeDetectorRef){
     
+    }
+    ngOnInit(): void {
+      this.getall();
+    }
+
+    getall(){
+      this.http.get<CustomerModel[]>("http://localhost:8080/customer/getall").subscribe(data=>{
+        console.log(data);
+        this.customerList=data;
+        this.cdr.detectChanges();
+        
+      })
+
+  }
 }
---
+++ src/model/type.ts
@@ -0,0 +1,15 @@
+export interface CustomerModel {
+    custID: string;
+    custTitle: string;
+    custName: string;
+    custAddress: string;
+    city: string;
+    province: string;
+    postalCode: string;
+    dob: Date;
+    salary: number;
+}
+
+export interface ItemModel{
+
+}
